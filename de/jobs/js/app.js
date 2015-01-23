e68f8f05229fe3b9d25ad343cc70fdcf896fe545
Max Stemplinger <ms@campudus.com>
2015-01-23 18:16:08 +0100

    form submit

--- de/jobs/js/app.js
+++ de/jobs/js/app.js
@@ -7,3 +7,52 @@ $(document).foundation({
   }
   
 });
+
+$(document).ready(function () {
+  var request;
+
+  $('#recruitment').submit(function(event) {
+    event.preventDefault();
+
+    // abort any pending request
+    if (request) {
+      request.abort();
+    }
+    var $form = $(this);
+    var serializedData = $form.serializeArray();
+
+    var skills = $(".tech-item.checked");
+    var skillArray = [];
+    skills.each(function () {
+      skillArray.push($(this).attr("id"));
+    });
+
+    serializedData.push({name : "skills", value : skillArray.join(",")});
+
+    request = $.ajax({
+      "url" : "https://script.google.com/macros/s/AKfycbwy_70uxGw1hsNbgXN0K9Lnjt2vFriM14qfdQmE0dIyNq6Vbjo/exec",
+      "content-type" : "application/json",
+      "type" : "post",
+      "data" : serializedData
+    });
+
+    request.done(function (response, textStatus, jqXHR) {
+      if (response.result == "success") {
+      } else {
+      }
+    });
+
+    // callback handler that will be called on failure
+    request.fail(function (jqXHR, textStatus, errorThrown) {
+      // log the error to the console
+      console.error(
+        "The following error occured: " +
+        textStatus, errorThrown
+      );
+    });
+
+    request.always(function () {
+    });
+
+  });
+});
