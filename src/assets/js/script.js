700d3aaf888ec13505d0c254e1b0f1124efe61fc
axtkonstantin@hotmail.de <e5,oqI,no>
2015-12-23 16:28:42 +0100

    add new campudus website

---
+++ src/assets/js/script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+
+  var $header = $('#hero');
+  var $heroImage = $header.find('.img-bg');
+  var $imprint = $('#imprint');
+
+  var $vh = $(window).height();
+
+  $imprint.css('height', $vh);
+
+  smoothScrolling();
+
+  $(window).on('load pagecontainerload', function () {
+    setViewPortProperties();
+  });
+
+  $(window).on('resize', function () {
+    if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+      setViewPortProperties();
+    }
+  });
+
+  $(window).on('orientationchange', function (event) {
+    var $viewportHeight = window.innerHeight;
+    $header.css('height', $viewportHeight);
+    //redraw
+    $('body').offset().top - 1;
+  });
+
+  function smoothScrolling() {
+    $('a[href*=#]:not([href=#])').click(function () {
+      console.log('CLICKED');
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          scrollTo(target.offset().top);
+          return false;
+        }
+      }
+    });
+  }
+
+  function scrollTo(position) {
+    $('html,body').animate({
+      scrollTop : Math.max(position, 0)
+    }, 1000);
+  }
+
+  function setViewPortProperties() {
+    var $viewportHeight = $(window).height();
+    var $viewportWidth = $(window).width();
+
+    if ($viewportWidth > 1920) {
+      $heroImage.css('width', $viewportWidth);
+      $header.css('height', $viewportHeight);
+    } else {
+      $header.css('height', $viewportHeight);
+    }
+  }
+
+});
