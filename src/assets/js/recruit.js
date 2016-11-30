b7346dc0ce0929281f88dc7eb7b913926b37fc66
konstantinaxt <axtkonstantin@hotmail.de>
2016-11-30 16:31:52 +0100

    refactor google analytics apply track function

--- src/assets/js/recruit.js
+++ src/assets/js/recruit.js
@@ -517,6 +517,7 @@ $(document).ready(function () {
     request.done(function (response, textStatus, jqXHR) {
       if (response.result == 'success') {
         $button.text(lang.success);
+        ga('send', 'event', 'Berechnung', 'gesendet');
       } else {
         console.error('The following error occured from script: ' + textStatus);
         resetAfterFail();
